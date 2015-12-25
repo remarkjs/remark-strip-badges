f635b761cfb92a695f966555d006221d02b453a0
Titus Wormer <tituswormer@gmail.com>
2015-12-25 13:16:06 +0100

    Refactor to replace mocha with tape

--- test.js
+++ test.js
@@ -8,62 +8,56 @@
 
 'use strict';
 
-/* eslint-env mocha */
+/* eslint-env node */
 
 /*
  * Dependencies.
  */
 
-var assert = require('assert');
+var test = require('tape');
 var remark = require('remark');
 var badges = require('./');
-
-/*
- * Fixtures.
- */
-
-var input = [
-    '# Hello',
-    '',
-    'non-badge: ![](http://example.com/fav.ico).',
-    '',
-    'https: ![build](https://img.shields.io/travis/joyent/node.svg).',
-    '',
-    'http: ![build](https://img.shields.io/coveralls/jekyll/jekyll.svg).',
-    '',
-    'reference: ![definition].',
-    '',
-    'nested: [![definition]](https://npm.im/localeval).',
-    '',
-    '[definition]: https://img.shields.io/scrutinizer/g/filp/whoops.svg'
-].join('\n') + '\n';
-
-var baseline = [
-    '# Hello',
-    '',
-    'non-badge: ![](http://example.com/fav.ico).',
-    '',
-    'https: .',
-    '',
-    'http: .',
-    '',
-    'reference: .',
-    '',
-    'nested: .',
-    '',
-    '[definition]: https://img.shields.io/scrutinizer/g/filp/whoops.svg'
-].join('\n') + '\n';
 
 /*
  * Tests.
  */
 
-describe('remark-strip-badges', function () {
-    it('should work', function (done) {
-        remark.use(badges).process(input, function (err, file, doc) {
-            done(err);
+test('remark-strip-badges', function (t) {
+    remark.use(badges).process([
+        '# Hello',
+        '',
+        'non-badge: ![](http://example.com/fav.ico).',
+        '',
+        'https: ![build](https://img.shields.io/travis/joyent/node.svg).',
+        '',
+        'http: ![build](https://img.shields.io/coveralls/jekyll/jekyll.svg).',
+        '',
+        'reference: ![definition].',
+        '',
+        'nested: [![definition]](https://npm.im/localeval).',
+        '',
+        '[definition]: https://img.shields.io/scrutinizer/g/filp/whoops.svg',
+        ''
+    ].join('\n'), function (err, file, doc) {
+        t.ifErr(err);
 
-            assert.equal(doc, baseline);
-        });
+        t.equal(doc, [
+            '# Hello',
+            '',
+            'non-badge: ![](http://example.com/fav.ico).',
+            '',
+            'https&#x3A; .',
+            '',
+            'http&#x3A; .',
+            '',
+            'reference: .',
+            '',
+            'nested: .',
+            '',
+            '[definition]: https://img.shields.io/scrutinizer/g/filp/whoops.svg',
+            ''
+        ].join('\n'));
+
+        t.end();
     });
 });
