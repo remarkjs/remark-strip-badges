36216622a7511bbd509b9a28a2bde69bb6d824b2
Titus Wormer <tituswormer@gmail.com>
2021-08-03 18:21:06 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
+/**
+ * @typedef {import('mdast').Root} Root
+ * @typedef {import('mdast').Content} Content
+ */
+
 import {definitions} from 'mdast-util-definitions'
 import {visit, SKIP} from 'unist-util-visit'
 import {isBadge} from 'is-badge'
 
+/**
+ * Plugin to strip badges (such as `shields.io`).
+ *
+ * @type {import('unified').Plugin<void[], Root>}
+ */
 export default function remarkStripBadges() {
-  return transformer
-}
+  return (tree) => {
+    const define = definitions(tree)
 
-function transformer(tree) {
-  const define = definitions(tree)
+    // Remove badge images, and links that include a badge image.
+    visit(tree, (node, index, parent) => {
+      let remove = false
 
-  visit(tree, check)
+      if (node.type === 'link' || node.type === 'linkReference') {
+        const children = node.children
+        let offset = -1
 
-  // Remove badge images, and links that include a badge image.
-  function check(node, index, parent) {
-    let remove = false
+        while (++offset < children.length) {
+          const child = children[offset]
 
-    if (node.type === 'link' || node.type === 'linkReference') {
-      const children = node.children
-      let offset = -1
-
-      while (++offset < children.length) {
-        const child = children[offset]
-
-        if (badgeImage(child, define)) {
-          remove = true
-          break
+          if (badgeImage(child, define)) {
+            remove = true
+            break
+          }
         }
+      } else if (badgeImage(node, define)) {
+        remove = true
       }
-    } else if (badgeImage(node, define)) {
-      remove = true
-    }
 
-    if (remove === true) {
-      parent.children.splice(index, 1)
-      return [SKIP, index]
-    }
+      if (remove === true && parent && typeof index === 'number') {
+        parent.children.splice(index, 1)
+        return [SKIP, index]
+      }
+    })
   }
 }
 
+/**
+ * @param {Root|Content} node
+ * @param {ReturnType<definitions>} define
+ * @returns {boolean|undefined}
+ */
 function badgeImage(node, define) {
   if (node.type === 'image') {
     return isBadge(node.url)
@@ -45,6 +56,6 @@ function badgeImage(node, define) {
 
   if (node.type === 'imageReference') {
     const def = define(node.identifier)
-    return isBadge(def.url)
+    return Boolean(def && isBadge(def.url))
   }
 }
