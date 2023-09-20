27cd2c134397bee04c6285ce123a014fe52a0157
Titus Wormer <tituswormer@gmail.com>
2023-09-20 17:27:55 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -1,73 +1,70 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {remark} from 'remark'
 import remarkStripBadges from './index.js'
 
-test('remark-strip-badges', (t) => {
-  t.plan(2)
+test('remark-strip-badges', async function (t) {
+  await t.test('should work', async function () {
+    const file = String(
+      await remark()
+        .use(remarkStripBadges)
+        .process(
+          [
+            '# Hello',
+            'non-badge: ![](http://example.com/fav.ico).',
+            'https: ![build](https://img.shields.io/travis/joyent/node.svg).',
+            'http: ![build](https://img.shields.io/coveralls/jekyll/jekyll.svg).',
+            'reference: ![definition].',
+            'nested: [![definition]](https://npm.im/localeval).',
+            'random: [other](https://link.com).',
+            '[definition]: https://img.shields.io/scrutinizer/g/filp/whoops.svg\n'
+          ].join('\n\n')
+        )
+    )
 
-  remark()
-    .use(remarkStripBadges)
-    .process(
+    assert.deepEqual(
+      String(file),
       [
         '# Hello',
         'non-badge: ![](http://example.com/fav.ico).',
-        'https: ![build](https://img.shields.io/travis/joyent/node.svg).',
-        'http: ![build](https://img.shields.io/coveralls/jekyll/jekyll.svg).',
-        'reference: ![definition].',
-        'nested: [![definition]](https://npm.im/localeval).',
+        'https: .',
+        'http: .',
+        'reference: .',
+        'nested: .',
         'random: [other](https://link.com).',
         '[definition]: https://img.shields.io/scrutinizer/g/filp/whoops.svg\n'
-      ].join('\n\n'),
-      (error, file) => {
-        t.deepEqual(
-          [error, String(file)],
+      ].join('\n\n')
+    )
+  })
+
+  await t.test('should work some more', async function () {
+    const file = String(
+      await remark()
+        .use(remarkStripBadges)
+        .process(
           [
-            undefined,
-            [
-              '# Hello',
-              'non-badge: ![](http://example.com/fav.ico).',
-              'https: .',
-              'http: .',
-              'reference: .',
-              'nested: .',
-              'random: [other](https://link.com).',
-              '[definition]: https://img.shields.io/scrutinizer/g/filp/whoops.svg\n'
-            ].join('\n\n')
-          ],
-          'should work'
+            '# remark-strip-badges [![Build Status][build-badge]][build-status] [![Coverage Status][coverage-badge]][coverage-status] [![Chat][chat-badge]][chat]',
+            '[build-badge]: https://img.shields.io/travis/remarkjs/remark-strip-badges.svg',
+            '[build-status]: https://travis-ci.org/remarkjs/remark-strip-badges',
+            '[coverage-badge]: https://img.shields.io/codecov/c/github/remarkjs/remark-strip-badges.svg',
+            '[coverage-status]: https://codecov.io/github/remarkjs/remark-strip-badges',
+            '[chat-badge]: https://img.shields.io/gitter/room/remarkjs/Lobby.svg',
+            '[chat]: https://gitter.im/remarkjs/Lobby\n'
+          ].join('\n\n')
         )
-      }
     )
 
-  remark()
-    .use(remarkStripBadges)
-    .process(
+    assert.deepEqual(
+      String(file),
       [
-        '# remark-strip-badges [![Build Status][build-badge]][build-status] [![Coverage Status][coverage-badge]][coverage-status] [![Chat][chat-badge]][chat]',
+        '# remark-strip-badges',
         '[build-badge]: https://img.shields.io/travis/remarkjs/remark-strip-badges.svg',
         '[build-status]: https://travis-ci.org/remarkjs/remark-strip-badges',
         '[coverage-badge]: https://img.shields.io/codecov/c/github/remarkjs/remark-strip-badges.svg',
         '[coverage-status]: https://codecov.io/github/remarkjs/remark-strip-badges',
         '[chat-badge]: https://img.shields.io/gitter/room/remarkjs/Lobby.svg',
         '[chat]: https://gitter.im/remarkjs/Lobby\n'
-      ].join('\n\n'),
-      (error, file) => {
-        t.deepEqual(
-          [error, String(file)],
-          [
-            undefined,
-            [
-              '# remark-strip-badges',
-              '[build-badge]: https://img.shields.io/travis/remarkjs/remark-strip-badges.svg',
-              '[build-status]: https://travis-ci.org/remarkjs/remark-strip-badges',
-              '[coverage-badge]: https://img.shields.io/codecov/c/github/remarkjs/remark-strip-badges.svg',
-              '[coverage-status]: https://codecov.io/github/remarkjs/remark-strip-badges',
-              '[chat-badge]: https://img.shields.io/gitter/room/remarkjs/Lobby.svg',
-              '[chat]: https://gitter.im/remarkjs/Lobby\n'
-            ].join('\n\n')
-          ],
-          'should work'
-        )
-      }
+      ].join('\n\n')
     )
+  })
 })
